4ab3fe8b212fe638587b39ade73d14c3255743b3
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-06-28 21:36:42 +0300

    Adjust container size.Create a div in js inside the container

--- eas.js
+++ eas.js
@@ -1,1 +1,6 @@
 console.log("Test");
+
+const container = document.querySelector('#container');
+const div1 = document.createElement('div');
+container.appendChild(div1);
+div1.setAttribute("style", "background:blue; height:64px; width: 64px;");
