6744c2a5db96ca38477fe3f0487d8639a3772c50
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-07-13 23:22:47 +0300

    Add a prompt requesting number of grids

--- eas.js
+++ eas.js
@@ -1386,26 +1386,57 @@ btn.addEventListener('click', () => {
 */
 
 const container = document.querySelector('#container');
-let row = 16;
-let column = 16;
-let cellSize = 20;
-container.style.width = `${column * cellSize}px`;
-container.style.fontSize = 0;
+//create a button to request for divs
+let btn = document.createElement('button');
+let body = document.querySelector('body');
+
+body.insertBefore(btn, container);
+btn.setAttribute('style', 'height: 20px; width: auto; alignItems: flexStart;');
+btn.textContent = "CLick Me!";
+
+function makeGrid(input) {
+    input = Number(prompt('Insert Number to create grid'));
+    let row = input;
+    let column = input;
+    let cellSize = 20;
+    container.style.width = `${column * cellSize}px`;
+    container.style.fontSize = 0;
 
 
-    for(let i=1; i<=row * column; i++) {
-        let cell = document.createElement('div');
-        cell.setAttribute('style', 'backgroundColor: green;');
-        cell.setAttribute(`class` ,`divs`);
-        cell.style.display = 'inline-block';
-        cell.style.width = `${cellSize}px`;
-        cell.style.height = '50px';
-        cell.style.border = '1px solid red';
-        container.appendChild(cell);
-     }
+        for(let i=1; i<=row * column; i++) {
+            let cell = document.createElement('div');
+            cell.setAttribute('style', 'backgroundColor: green;');
+            cell.setAttribute(`class` ,`divs`);
+            cell.style.display = 'inline-block';
+            cell.style.width = `${cellSize}px`;
+            cell.style.height = '50px';
+            cell.style.border = '1px solid red';
+            container.appendChild(cell);
+        }
+
+    //add a mouse down event listener(hover effect)
+        let divs = document.querySelectorAll(".divs");
+
+        let divArr = [...divs];
+        divArr.forEach(
+            div => {
+                div.addEventListener('mouseover', () => {
+                    div.style.background = 'blue';
+                }
+                );
+                
+                div.addEventListener('mouseout', () => {
+                    div.style.background = 'lightGrey';
+                }
+                );
+            }
+        );
+}
+
+btn.addEventListener('click', makeGrid);
+
 
 //add a mouse down event listener(hover effect)
-let press = document.querySelector("#container");
 let divs = document.querySelectorAll(".divs");
 
 let divArr = [...divs];
